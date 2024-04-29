7f632ea4c61ac8fa241b45f0e8a61978eef12874
enzo antilipi <enzoantilipi1@gmail.com>
2024-04-28 23:39:17 -0300

    primer push

---
+++ src/api/afiliado/controllers/afiliado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * afiliado controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::afiliado.afiliado');
---
+++ src/api/afiliado/services/afiliado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * afiliado service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::afiliado.afiliado');
